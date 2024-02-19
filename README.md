f52ee480a642aa544ae7bb545f28bc8e1e11ba8f
ginskyz <157693868+ginskyz@users.noreply.github.com>
2024-02-19 15:13:50 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LEVEL-1_
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/ginskyz/LEVEL-1_)
---
+++ index.js
@@ -0,0 +1,25 @@
+// run `node index.js` in the terminal
+var nama = "Gina Pateda";
+var umur = 27;
+var alamat = "Jl. Prapanca No. 6";
+var hobi = "Membaca Buku";
+var asal = "Univ. Negeri Gorontalo";
+var why = "Fleksibilitas Karir";
+var dipelajari = "Java dan HTML";
+var kesulitan = "Pernah";
+var alasan = "Persiapan Karir";
+var terima = "Minat belajar yang tinggi";
+
+
+console.log(nama);
+console.log(umur);
+console.log(alamat);
+console.log(hobi);
+console.log(asal);
+console.log(why);
+console.log(dipelajari);
+console.log(kesulitan);
+console.log(alasan);
+console.log(terima);
+
+
